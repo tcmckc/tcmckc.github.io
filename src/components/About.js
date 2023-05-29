3f7c50760c0fe13de57e69633909ea48279b0593
Tomoko Ishii <tomoko.s.ishii@gmail.com>
2023-05-29 22:32:45 +0200

    Updated About

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,8 @@ function About() {
         <div className="about-container">
             <h3>ABOUT</h3>
             <div className="about-item">
-                <p>My name is Tomoko, from Tokyo living in Stockholm. I am a mom and former humanitarian worker who loves photography, travel and fika.</p>
+                <p>My name is Tomoko, from Tokyo living in Stockholm. 
+                I will soon graduate from Blekinge Institute of Technology with a degree in Software Engineering. Besides programming, I am a mom and former humanitarian worker who loves photography, travel and fika.</p>
                 <img
                 className="about-photo"
                 src="./../img/profile.jpg"
